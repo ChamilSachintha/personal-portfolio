61f61d71945b5cb8c5b296993f7b5142b896b27e
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2022-11-22 13:17:39 +0530

    New project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,28 +2,30 @@ import { Container, Row, Tab, Col, Nav } from "react-bootstrap";
 import { ProjectCard1 } from "./ProjectCards/ProjectCard1";
 import { ProjectCard2 } from "./ProjectCards/ProjectCard2";
 import { ProjectCard3 } from "./ProjectCards/ProjectCard3";
+import { ProjectCard4 } from "./ProjectCards/ProjectCard4";
 import TrackVisibility from 'react-on-screen';
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
 
 export const Projects = () => {
 
     const Projects = [
         {
             title1: "Pharmacy Management System",
-            description1: "A database management system by using HTML5, CSS3 & PHP",
+            description1: "Full-stack application | HTML5, CSS3 & PHP",
             imgUrl1: projImg1,
         },
         {
             title1: "Pharmacy Management System",
-            description1: "A database management system by using HTML5, CSS3 & PHP",
+            description1: "Full-stack application | HTML5, CSS3 & PHP",
             imgUrl1: projImg1,
         },
         {
             title1: "Pharmacy Management System",
-            description1: "A database management system by using HTML5, CSS3 & PHP",
+            description1: "Full-stack application | HTML5, CSS3 & PHP",
             imgUrl1: projImg1,
         },
         {
@@ -56,7 +58,23 @@ export const Projects = () => {
             title3: "Academic Calendar for Universities",
             description3: "Figma UI/UX design for a web application",
             imgUrl3: projImg3,
-        }
+        },
+
+        {
+            title4: "Personal Portfolio Website",
+            description4: "Front-end application | React, React-bootstrap & Animate.css",
+            imgUrl4: projImg4,
+        },
+        {
+            title4: "Personal Portfolio Website",
+            description4: "Front-end application | React, React-bootstrap & Animate.css",
+            imgUrl4: projImg4,
+        },
+        {
+            title4: "Personal Portfolio Website",
+            description4: "Front-end application | React, React-bootstrap & Animate.css",
+            imgUrl4: projImg4,
+        },
 
     ]
     return (
@@ -65,10 +83,10 @@ export const Projects = () => {
                 <Row>
                     <Col>
                         <TrackVisibility>
-                            {({ isVisible }) => 
-                            <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                        <h2>Projects</h2>
-                        </div>}
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                                    <h2>Projects</h2>
+                                </div>}
                         </TrackVisibility>
                         <p>“Every project is an opportunity to learn, figure out problems and challenges, and invent and reinvent.”</p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
@@ -85,46 +103,67 @@ export const Projects = () => {
                             </Nav>
                             <Tab.Content>
                                 <Tab.Pane eventKey="first">
-                                    <Row>
-                                        {
-                                            Projects.map((project, index) => {
-                                                return (
-                                                    <ProjectCard1
-                                                        key={index}
-                                                        {...project}
-                                                    />
-                                                )
-                                            })
-                                        }
-                                    </Row>
+                                    <a href="https://github.com/ChamilSachintha/Pharmacy-Management-System" target="_blank">
+                                        <Row>
+                                            {
+                                                Projects.map((project, index) => {
+                                                    return (
+                                                        <ProjectCard1
+                                                            key={index}
+                                                            {...project}
+                                                        />
+                                                    )
+                                                })
+                                            }
+                                        </Row>
+                                    </a>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="second">
-                                    <Row class>
-                                        This section will update soon!
-                                    </Row>
+                                    <a href="https://github.com/ChamilSachintha/Pharmacy-Management-System" target="_blank">
+                                        <Row>
+                                            {
+                                                Projects.map((project, index) => {
+                                                    return (
+                                                        <ProjectCard4
+                                                            key={index}
+                                                            {...project}
+                                                        />
+                                                    )
+                                                })
+                                            }
+                                        </Row>
+                                    </a>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="third">
                                     <Row>
-                                        {
-                                            Projects.map((project, index) => {
-                                                return (
-                                                    <ProjectCard2
-                                                        key={index}
-                                                        {...project}
-                                                    />
-                                                )
-                                            })
-                                        }
-                                        {
-                                            Projects.map((project, index) => {
-                                                return (
-                                                    <ProjectCard3
-                                                        key={index}
-                                                        {...project}
-                                                    />
-                                                )
-                                            })
-                                        }
+                                        <a href="https://www.figma.com/file/08BeH4Xv1yw2M819bzyFzS/Design-1?t=jZK3aOJNwAHjyEXN-1" target="_blank">
+                                            <Row>
+                                                {
+                                                    Projects.map((project, index) => {
+                                                        return (
+                                                            <ProjectCard2
+                                                                key={index}
+                                                                {...project}
+                                                            />
+                                                        )
+                                                    })
+                                                }
+                                            </Row>
+                                        </a>
+                                        <a href="https://www.figma.com/file/IPv7RZ6prAhNObLnDxr76Z/UI%2FUX-Design-for-Academic-Calendar?node-id=0%3A1&t=jZK3aOJNwAHjyEXN-1" target="_blank">
+                                            <Row>
+                                                {
+                                                    Projects.map((project, index) => {
+                                                        return (
+                                                            <ProjectCard3
+                                                                key={index}
+                                                                {...project}
+                                                            />
+                                                        )
+                                                    })
+                                                }
+                                            </Row>
+                                        </a>
                                     </Row>
                                 </Tab.Pane>
                             </Tab.Content>
